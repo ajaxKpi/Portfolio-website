0690015919f315b00f5f25ba7957861163732796
Ivan <WorkWin7@mail.com>
2015-08-12 16:58:31 +0300

    Add preloader with JS

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by zvorskyi on 12.08.2015.
+ */
+$(window).load(function() {
+
+
+    $(".typing-indicator").delay(400).fadeOut("slow");
+    $(".loader-wrapper").delay(400).fadeOut("slow");
+
+});
