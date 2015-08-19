c21ca119d1837144a9ae0b0ffe73431d492230b8
Ivan <WorkWin7@mail.com>
2015-08-19 18:05:33 +0300

    Add social bars

--- js/main.js
+++ js/main.js
@@ -29,13 +29,25 @@ var autoSlide = setInterval(function() {
              prevIndex = currentIndex;
 }, 4000);
 
+
+//call pin button on specific location
+
+$('.Blog_photo img').imgPin(
+    {pinImg : 'img/social/pinterestOnImg.png', position: 2
+    }
+
+);
+
+
+
+/*
 Arr_img = getImagesByAlt("Blog_photo");
-console.log(5 + " and " + Arr_img);
+
 for (one_img in Arr_img){
     ImgHeigt =Arr_img[one_img].height;
     ImgWith =Arr_img[one_img].width;
 
-console.log(ImgWith + "  " + ImgHeigt)
+
     if (ImgHeigt>ImgWith){
 
         $(".Main_content").find(Arr_img[one_img]).parent().css("width","49%")
@@ -58,3 +70,42 @@ function getImagesByAlt(alt) {
     }
     return images;
 }
+*/
+
+
+Share = {
+    vkontakte: function(purl, ptitle, pimg, text) {
+        url  = 'http://vkontakte.ru/share.php?';
+        url += 'url='          + encodeURIComponent(purl);
+        url += '&title='       + encodeURIComponent(ptitle);
+        url += '&description=' + encodeURIComponent(text);
+        url += '&image='       + encodeURIComponent(pimg);
+        url += '&noparse=true';
+        Share.popup(url);
+    },
+
+    facebook: function(purl, ptitle, pimg, text) {
+        url  = 'http://www.facebook.com/sharer.php?s=100';
+        url += '&p[title]='     + encodeURIComponent(ptitle);
+        url += '&p[summary]='   + encodeURIComponent(text);
+        url += '&p[url]='       + encodeURIComponent(purl);
+        url += '&p[images][0]=' + encodeURIComponent(pimg);
+        Share.popup(url);
+    },
+
+    popup: function(url) {
+        window.open(url,'','toolbar=0,status=0,width=626,height=436');
+    }
+};
+
+// paste fb comment bar for custom language
+var userLang = navigator.language || navigator.userLanguage;
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_EN/sdk.js#xfbml=1&version=v2.4";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+$("#f197705904").remove();
