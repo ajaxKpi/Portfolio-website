6a8af356f3d57df779f084dfc19d68f893c05128
Ivan <Ivan@mymail.com>
2016-07-10 13:34:43 +0300

    fail

--- app/app.js
+++ app/app.js
@@ -76,7 +76,8 @@ angular.module('myApp', [
           redirectTo: "/"
       });
 
-        $locationProvider.html5Mode(true);
+        $locationProvider.html5Mode({enabled: true, requireBase: false});
+
 
 }])
     .run( ['$FB','$rootScope',function($FB,$rootScope){
